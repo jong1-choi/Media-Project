cd86b46478de5426f3b854f553b934fe113ceb1e
InYongShin <tlsdlsdyd98@gmail.com>
2021-12-09 13:59:17 +0900

    Test Build

--- Assets/Scripts/DetectEnemy.cs
+++ Assets/Scripts/DetectEnemy.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using MediaProject;
-using TreeEditor;
+// using TreeEditor;
 using UnityEngine;
 
 public class DetectEnemy : MonoBehaviour
